7fe890728fa79f99d19417556edc9ee15ab3fad6
Nora Kauczor <n.kauczor@web.de>
2024-10-13 17:50:19 +0200

    add routes for homepage, calendar and review page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage.tsx";
+import ReviewPage from "./pages/ReviewPage/ReviewPage.tsx";
+import CalendarPage
+    from "./pages/CalendarPage/CalendarPage.tsx";
 
 function App() {
 
 
   return (
     <>
-      <
+      <Routes>
+          <Route path={"/"} element={<HomePage/>}></Route>
+          <Route path={"/calendar"} element={<CalendarPage vocabs={vocabs}/>}></Route>
+          <Route path={"/review"} element={<ReviewPage vocabsOfTheDay={vocabsOfTheDay}/>}></Route>
+      </Routes>
     </>
   )
 }
