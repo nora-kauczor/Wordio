d96370e7cb9c1ac311f765f27fb4905969946492
Nora Kauczor <n.kauczor@web.de>
2024-10-14 17:54:32 +0200

    move get all function to calendar page

--- frontend/src/pages/ReviewPage/ReviewPage.tsx
+++ frontend/src/pages/ReviewPage/ReviewPage.tsx
@@ -1,11 +1,9 @@
 import './ReviewPage.css'
-import {Vocab} from "../../types/Vocab.ts";
 
-type Props = {
-    vocabsOfToday:Vocab[]
-}
 
-export default function ReviewPage(props: Readonly<Props>){return(
+
+
+export default function ReviewPage(){return(
     <div id={"review-page"}>
 
     </div>
