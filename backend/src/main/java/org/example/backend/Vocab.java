192374334a101e04dcd38d691dc9f9cd61c33cfb
Nora Kauczor <n.kauczor@web.de>
2024-10-15 10:46:09 +0200

    write create method in service and test it successfully

--- backend/src/main/java/org/example/backend/Vocab.java
+++ backend/src/main/java/org/example/backend/Vocab.java
@@ -1,9 +1,11 @@
 package org.example.backend;
 
+import org.springframework.data.annotation.Id;
+
 import java.util.Date;
 import java.util.List;
 
-public record Vocab(String _id,
+public record Vocab(@Id String _id,
                     String word,
                     String translation,
                     String info,
--- backend/src/main/java/org/example/backend/VocabService.java
+++ backend/src/main/java/org/example/backend/VocabService.java
@@ -13,4 +13,10 @@ public class VocabService {
     public List<Vocab> getAllVocabs() {
         return vocabRepo.findAll();
     }
+
+    public Vocab createVocab(VocabDTO vocabDTO){
+        Vocab newVocab = new Vocab(null, vocabDTO.word(), vocabDTO.translation(),
+                vocabDTO.info(), vocabDTO.language(), vocabDTO.reviewDates());
+        return vocabRepo.save(newVocab);
+    }
 }
