730fa1be0e61402c2590e2bc87968505919e03e1
Nora Kauczor <n.kauczor@web.de>
2024-10-16 15:01:41 +0200

    move vocab state up to app.tsx and pass it down to pages

--- frontend/src/pages/BacklogPage/BacklogPage.tsx
+++ frontend/src/pages/BacklogPage/BacklogPage.tsx
@@ -3,6 +3,7 @@ import './BacklogPage.css'
 import {Vocab} from "../../types/Vocab.ts";
 type Props = {
     vocabs:Vocab[]
+    deleteVocab:(_id:string) => void
 }
 
 export default function BacklogPage(props:Readonly<Props>){
