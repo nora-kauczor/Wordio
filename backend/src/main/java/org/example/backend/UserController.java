6840b0c9d8e6d8b4c8d32f414c130d1bdaf242dd
Nora Kauczor <n.kauczor@web.de>
2024-10-17 16:52:01 +0200

    create AppUser record and UserRepo interface

---
+++ backend/src/main/java/org/example/backend/UserController.java
@@ -0,0 +1,27 @@
+package org.example.backend;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/vocab/auth")
+public class UserController {
+
+    private final UserRepo userRepo;
+
+
+    @GetMapping
+    public AppUser getUser(@AuthenticationPrincipal OAuth2User user) {
+        if
+       ( user == null) {
+            return new AppUser("NotFound", "anonymousUser", null, null);
+        }
+               return userRepo.findById(user.getName()).orElseThrow();
+    }
+
+}
