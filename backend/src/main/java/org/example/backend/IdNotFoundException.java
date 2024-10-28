926299dea4e171c53632a36321116088848d456b
Nora Kauczor <n.kauczor@web.de>
2024-10-28 18:08:47 +0100

    write custom exceptions and use them in methods incl. tests

---
+++ backend/src/main/java/org/example/backend/IdNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.backend;
+
+public class IdNotFoundException extends Exception {
+    public IdNotFoundException(String message) {
+        super(message);
+    }
+}
--- backend/src/test/java/org/example/backend/LanguageTest.java
+++ backend/src/test/java/org/example/backend/LanguageTest.java
@@ -2,19 +2,16 @@ package org.example.backend;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class LanguageTest {
 
     @Test
-    void getEnumByString_shouldThrowNoSuchElementException_whenCalledWithNonexistentLanguage() {
-        assertThrows(NoSuchElementException.class, () -> Language.getEnumByString("Esperanto"));
+    void getEnumByString_shouldThrowLanguageNotFoundException_whenCalledWithNonexistentLanguage() {
+        assertThrows(LanguageNotFoundException.class, () -> Language.getEnumByString("Esperanto"));
     }
     @Test
-    void getEnumByString_shouldReturnSPANISHEnum_whenCalledWithStringSpanish() {
+    void getEnumByString_shouldReturnSPANISHEnum_whenCalledWithStringSpanish() throws LanguageNotFoundException {
         Language expected = Language.SPANISH;
         Language actual= Language.getEnumByString("Spanish");
                 assertEquals(expected, actual);
