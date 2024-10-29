1d8b56169d37aeb7fc2e9f871bbc96e13ea1fac0
Nora Kauczor <n.kauczor@web.de>
2024-10-29 14:41:28 +0100

    override toString, equals and hashCode in Month record as demanded by sonar cloud

--- backend/src/main/java/org/example/backend/calendar/Month.java
+++ backend/src/main/java/org/example/backend/calendar/Month.java
@@ -1,6 +1,32 @@
 package org.example.backend.calendar;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record Month
         (String yearMonthName,
          VocabIdsOfDate[][] vocabIdsOfMonth) {
+
+    @Override
+    public String toString() {
+        return "Month{" +
+               "vocabIdsOfMonth=" + Arrays.toString(vocabIdsOfMonth) +
+               ", yearMonthName='" + yearMonthName + '\'' +
+               '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Month month = (Month) o;
+        return Objects.equals(yearMonthName, month.yearMonthName) && Objects.deepEquals(vocabIdsOfMonth, month.vocabIdsOfMonth);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(yearMonthName, Arrays.deepHashCode(vocabIdsOfMonth));
+    }
 }
