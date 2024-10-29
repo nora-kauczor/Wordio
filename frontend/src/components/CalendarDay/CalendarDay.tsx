18059a614650bd32e9faaff8a3ec17148d8163df
Nora Kauczor <n.kauczor@web.de>
2024-10-29 14:11:18 +0100

    use icons in calendar days to display number of vocabs

--- frontend/src/components/CalendarDay/CalendarDay.tsx
+++ frontend/src/components/CalendarDay/CalendarDay.tsx
@@ -2,7 +2,9 @@ import {
     VocabIdsOfDate
 } from "../../types/VocabsIdsOfDate.ts";
 import './CalendarDay.css'
-import { RxDotFilled } from "react-icons/rx";
+
+import {GoDotFill} from "react-icons/go";
+import {FaPlus} from "react-icons/fa";
 
 
 type Props = {
@@ -11,18 +13,33 @@ type Props = {
 }
 
 export default function CalendarDay(props: Readonly<Props>) {
-    const displayDate:string = props.vocabIdsOfDate?.date?.substring(8, 10)+"."
-        +props.vocabIdsOfDate?.date?.substring(5,7)+"."
+    const displayDate: string = props.vocabIdsOfDate?.date?.substring(8, 10) + "."
+        + props.vocabIdsOfDate?.date?.substring(5, 7) + "."
 
-console.log(props.vocabIdsOfDate)
     return (
         <div id={"calendar-day-wrapper"}>
-            {props.vocabIdsOfDate && <button id={"calendar-day"}
-                     onClick={() => props.vocabIdsOfDate && props.openDayPopUpAndPassItVocabs(props.vocabIdsOfDate)}
-                     onKeyDown={() => props.vocabIdsOfDate && props.openDayPopUpAndPassItVocabs(props.vocabIdsOfDate)}>
-                <p> {displayDate}</p>
-                <p>{props.vocabIdsOfDate?.vocabIds?.length} Vocabs</p>
-            </button>}
+            {props.vocabIdsOfDate &&
+                <button id={"calendar-day"} className={props.vocabIdsOfDate?.vocabIds?.length > 0 ? 'dots' : 'no-dots'}
+                        onClick={() => props.vocabIdsOfDate && props.openDayPopUpAndPassItVocabs(props.vocabIdsOfDate)}
+                        onKeyDown={() => props.vocabIdsOfDate && props.openDayPopUpAndPassItVocabs(props.vocabIdsOfDate)}>
+                    <p id={"calendar-date"} > {displayDate}</p>
+                    <div id={"dot-container"}>
+                        {props.vocabIdsOfDate?.vocabIds?.length > 0 ? <GoDotFill id={"dot-1"}
+                                   className={"dot"}/> : <GoDotFill id={"invisible-dot"}
+                                                                    className={"dot"}/>}
+                        {props.vocabIdsOfDate?.vocabIds?.length > 1 && <GoDotFill id={"dot-2"}
+                                   className={"dot"}/>}
+                        {props.vocabIdsOfDate?.vocabIds?.length > 2 && <GoDotFill
+                        id={"dot-3"}
+                        className={"dot"}/>}
+                        {props.vocabIdsOfDate?.vocabIds?.length > 3 && <GoDotFill
+                        id={"dot-4"}
+                        className={"dot"}/>}
+                        {props.vocabIdsOfDate?.vocabIds?.length > 4 && <GoDotFill
+                        id={"dot-5"} className={"dot"}/>}
+                        {props.vocabIdsOfDate?.vocabIds?.length > 5 &&  <FaPlus id={"plus"} className={"dot"}/>}
+                    </div>
+                </button>}
         </div>
     )
 }
