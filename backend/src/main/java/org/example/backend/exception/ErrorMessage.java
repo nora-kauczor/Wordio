1350983352f43c134fabf8167f7509bfcc0123e4
Nora Kauczor <n.kauczor@web.de>
2024-11-01 09:44:20 +0100

    delete error message class and adjust global exception handler

--- backend/src/main/java/org/example/backend/exception/ErrorMessage.java
+++
@@ -1,7 +0,0 @@
-package org.example.backend.exception;
-
-
-
-public record ErrorMessage(String message) {
-
-}
--- backend/src/main/java/org/example/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/org/example/backend/exception/GlobalExceptionHandler.java
@@ -11,17 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler({IdNotFoundException.class, LanguageNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorMessage handleNotFoundExceptions(Exception exception) {
-       ErrorMessage errorMessage = new ErrorMessage(exception.getMessage());
-        System.out.println(errorMessage.message());
-       return errorMessage;
+    public String handleNotFoundExceptions(Exception exception) {
+        System.out.println(exception.getMessage());
+       return exception.getMessage();
     }
 
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(VocabIsNotEditableException.class)
-    public ErrorMessage handleVocabIsNotEditableException(Exception exception) {
-        ErrorMessage errorMessage = new ErrorMessage(exception.getMessage());
-        System.out.println(errorMessage.message());
-        return errorMessage;
+    public String handleVocabIsNotEditableException(Exception exception) {
+        System.out.println(exception.getMessage());
+        return exception.getMessage();
     }
 }
