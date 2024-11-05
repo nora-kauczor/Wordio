907e5a79455d400556d051adba9b8405ffb031ff
Nora Kauczor <n.kauczor@web.de>
2024-11-05 22:25:49 +0100

    add aria label in LoginPage

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,20 +1,18 @@
 import './LoginPage.css'
 
 
-
-
 export default function LoginPage() {
 
     function login() {
         const host = window.location.host === "localhost:5173" ?
-            "http://localhost:8080"
-            : window.location.origin
+            "http://localhost:8080" : window.location.origin
         window.open(host + "/oauth2/authorization/github", "_self")
     }
 
-    return (
-        <div id={"login-page"}>
-            <button id={"login-button"} onClick={login} onKeyDown={login}>Login</button>
-        </div>
-    )
+    return (<div id={"login-page"}>
+            <button id={"login-button"}
+                    onClick={login}
+                    aria-label={"Login with GitHub"}>Login
+            </button>
+        </div>)
 }
