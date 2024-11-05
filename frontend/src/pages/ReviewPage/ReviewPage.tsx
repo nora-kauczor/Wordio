2697df01d9810ba1979bb50723c055cbe80a1dfa
Nora Kauczor <n.kauczor@web.de>
2024-11-05 23:35:18 +0100

    write getInputWithoutExtraSpaces function in ReviewPage

--- frontend/src/pages/ReviewPage/ReviewPage.tsx
+++ frontend/src/pages/ReviewPage/ReviewPage.tsx
@@ -1,7 +1,6 @@
 import './ReviewPage.css'
 import {useEffect, useState} from "react";
-import CardContainer
-    from "../../components/CardContainer/CardContainer.tsx";
+import CardContainer from "../../components/CardContainer/CardContainer.tsx";
 import {Vocab} from "../../types/Vocab.ts";
 import {useNavigate} from "react-router-dom";
 import Confetti from 'react-confetti';
@@ -14,46 +13,74 @@ type Props = {
 }
 
 export default function ReviewPage(props: Readonly<Props>) {
+
     const [currentIndex, setCurrentIndex] = useLocalStorageState("currentIndex",
         {defaultValue: 0});
     const [currentVocab, setCurrentVocab] = useLocalStorageState("currentVocab",
         {defaultValue: props.vocabsLeftToReview[0]});
     const [userInput, setUserInput] = useState<string>("")
     const [showFireworks, setShowFireworks] = useState(false);
-const navigate = useNavigate()
+    const navigate = useNavigate()
 
     useEffect(() => {
         setCurrentVocab(props.vocabsLeftToReview[currentIndex])
     }, [currentIndex]);
 
+    function getInputWithoutExtraSpaces(userInput: string): string {
+        const chars: string[] = userInput.split('');
+        let charsWithoutExtraSpaces: string[] = chars;
+        let i: number = 0;
+        while (charsWithoutExtraSpaces[i] === " ") {
+            charsWithoutExtraSpaces.splice(i, 1)
+        }
+        let k = charsWithoutExtraSpaces.length - 1
+        while (chars[k] === " ") {
+            charsWithoutExtraSpaces.splice(k, 1)
+        }
+        let z: number = 0;
+        while (z < charsWithoutExtraSpaces.length) {
+            if (charsWithoutExtraSpaces[z] === " " &&
+                charsWithoutExtraSpaces[z + 1] === " ") {
+                charsWithoutExtraSpaces.splice(z + 1, 1)
+            } else {
+                z++
+            }
+
+        }
+        return charsWithoutExtraSpaces.reduce((a, b) => a + b)
+    }
+
     function checkAnswer() {
-        // inputWithoutExtraSpaces
-        if (currentVocab.word.toLowerCase() !== userInput.toLowerCase()) {
+        const inputWithoutExtraSpaces: string = getInputWithoutExtraSpaces(
+            userInput)
+        if (currentVocab.word.toLowerCase() !==
+            inputWithoutExtraSpaces.toLowerCase()) {
             props.changeReviewDates(currentVocab._id)
             navigate(`/display:${currentVocab._id}`)
+        } else {
+            setShowFireworks(true);
+            setTimeout(() => {
+                setShowFireworks(false);
+            }, 2500);
+            getNextVocab()
         }
-        else {
-                setShowFireworks(true);
-                setTimeout(() => {
-                    setShowFireworks(false);
-                }, 2500);
-            getNextVocab()}
         props.removeVocabFromVocabsToReview(currentVocab._id)
     }
 
-    function getNextVocab():void{
+    function getNextVocab(): void {
         setCurrentIndex(currentIndex + 1)
     }
 
     return (<div id={"review-page"} role={"main"}>
-            {showFireworks && <Confetti/>}
-            {currentVocab && <CardContainer displayedVocab={currentVocab}/>}
-            <label htmlFor={"review-input"} className={"visually-hidden"}>Your
-                answer</label>
-            <input id={"review-input"}
-                   onChange={element => setUserInput(element.target.value)}
-                   aria-label={"Enter your answer"}
-                   placeholder={"Type your answer here"}/>
-            <button onClick={checkAnswer}aria-label={"Submit your answer"}>submit</button>
-        </div>)
+        {showFireworks && <Confetti/>}
+        {currentVocab && <CardContainer displayedVocab={currentVocab}/>}
+        <label htmlFor={"review-input"} className={"visually-hidden"}>Your
+            answer</label>
+        <input id={"review-input"}
+               onChange={element => setUserInput(element.target.value)}
+               aria-label={"Enter your answer"}
+               placeholder={"Type your answer here"}/>
+        <button onClick={checkAnswer} aria-label={"Submit your answer"}>submit
+        </button>
+    </div>)
 }
