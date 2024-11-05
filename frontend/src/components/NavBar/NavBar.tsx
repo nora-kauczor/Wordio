503d9dd0e7efdc3404d5607b7ad9eeda03c366d8
Nora Kauczor <n.kauczor@web.de>
2024-11-05 21:42:02 +0100

    apply aria labels etc. in NavBar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,32 +1,40 @@
 import {useNavigate} from "react-router-dom";
 import './NavBar.css'
+import React from "react";
 
 type Props = {
     setUseForm: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+
 export default function NavBar(props: Readonly<Props>) {
     const navigate = useNavigate();
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLLIElement>,
+                           path: string) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            navigate(path);
+        }
+    };
     return (
         <ul id={"navbar"}>
             <li  onClick={() =>
                 navigate('/backlog')}
-                onKeyDown={() =>
-                    navigate('/backlog')
-                }>Backlog
+                 onKeyDown={(event) => handleKeyDown(event, '/backlog')}
+                 aria-label={"Go to Backlog"}
+            >Backlog
             </li>
             <li
                 onClick={() => props.setUseForm(true)}
-                onKeyDown={() => props.setUseForm(true)}
+                onKeyDown={(event) => handleKeyDown(event, '')}
+                aria-label={"Create a new item"}
             >Create
             </li>
             <li
                 onClick={() =>
                     navigate('/calendar')
                 }
-                onKeyDown={() =>
-                    navigate('/calendar')
-                }
+                onKeyDown={(event) => handleKeyDown(event, '/calendar')}
+                aria-label={"Go to Calendar"}
             >Calendar
             </li>
         </ul>
