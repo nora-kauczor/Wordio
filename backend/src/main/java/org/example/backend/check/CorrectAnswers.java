12faa0385b7d6e43a8036e8fe4644350e3e65780
Nora Kauczor <n.kauczor@web.de>
2024-11-19 11:23:04 +0100

    write getWordWithoutArticle method and test it successfully

--- backend/src/main/java/org/example/backend/check/CorrectAnswers.java
+++ backend/src/main/java/org/example/backend/check/CorrectAnswers.java
@@ -1,6 +1,10 @@
 package org.example.backend.check;
 
+import org.example.backend.vocab.Language;
+
 import java.util.List;
+
+
 
 public class CorrectAnswers {
 
@@ -22,30 +26,67 @@ public class CorrectAnswers {
     public static String getLeftSideOfSlash(String word) {
         char slash = '/';
         boolean wordHasSlash = word.indexOf(slash) != -1;
-        if (!wordHasSlash) {return word;}
+        if (!wordHasSlash) {
+            return null;
+        }
         int indexOfSlash = word.indexOf(slash);
-        return word.substring(0,indexOfSlash);
+        return word.substring(0, indexOfSlash);
     }
 
     public static String getRightSideOfSlash(String word) {
         char slash = '/';
         boolean wordHasSlash = word.indexOf(slash) != -1;
-        if (!wordHasSlash) {return word;}
+        if (!wordHasSlash) {
+            return null;
+        }
         int indexOfSlash = word.indexOf(slash);
-        return word.substring(indexOfSlash+1);
+        return word.substring(indexOfSlash + 1);
     }
 
-    // getLeftSideWithEndingOfRightSide
+    public static String getLeftSideOfSlashWithEndingOfRightSide(String word) {
+        char slash = '/';
+        boolean wordHasSlash = word.indexOf(slash) != -1;
+        if (!wordHasSlash) {
+            return null;
+        }
+        int indexOfSlash = word.indexOf(slash);
+        char minus = '-';
+        boolean wordHasMinus = word.indexOf(minus) != -1;
+        if (!wordHasMinus) {
+            return null;
+        }
+        String ending = word.substring(indexOfSlash + 2);
+        String leftSideWithoutLastLetter = word.substring(0, indexOfSlash - 1);
+        return leftSideWithoutLastLetter + ending;
+    }
 
-//    public static String getWordWithoutArticle(String word) {
-//         List<String> spanishArticles = List.of("el", "la", "los", "las", "un",
-//                "una", "unos", "unas");
-//        List<String> frenchArticles = List.of("le", "la", "les", "un", "une", "des",
-//                "l'");
-//        List<String> italianArticles = List.of("il", "lo", "la", "i", "gli", "le",
-//                "un", "uno", "una", "un'");
-//
-//    }
+    public static String getWordWithoutArticle(String word, Language language) {
+        List<String> articles = List.of("el", "la", "los", "las", "un",
+                "una", "unos", "unas");
+        if (language.equals(Language.ITALIAN)) {
+            articles = List.of("il", "lo", "la", "i", "gli", "le",
+                    "un", "uno", "una", "un'");
+        }
+        if (language.equals(Language.FRENCH)) {
+            articles = List.of("le", "la", "les", "un", "une", "des",
+                    "l'");
+        }
+        boolean wordStartsWithArticlesLetters = articles.stream().anyMatch(word::contains);
+        if (!wordStartsWithArticlesLetters) {
+            return null;
+        }
+        String wordWithoutArticle = null;
+        for (String article : articles) {
+            boolean articleIsAtBeginning = word.indexOf(article) == 0;
+            int articleLength = article.length();
+            String substringBehindArticle = word.substring(articleLength, articleLength + 1);
+            boolean spaceBehindArticle = substringBehindArticle.equals(" ");
+            if (articleIsAtBeginning && spaceBehindArticle) {
+                wordWithoutArticle = word.substring(articleLength + 1);
+            }
+        }
+        return wordWithoutArticle;
+    }
 
 //    public static String getWordWithoutBrackets(String word) {
 //    }
--- backend/src/test/java/org/example/backend/check/CorrectAnswersTest.java
+++ backend/src/test/java/org/example/backend/check/CorrectAnswersTest.java
@@ -1,14 +1,14 @@
 package org.example.backend.check;
 
+import org.example.backend.vocab.Language;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CorrectAnswersTest {
 
-
     @Test
-    void getLeftSideOfSlash() {
+    void getLeftSideOfSlash_shouldReturnBeau_whenCalledWithBeauBelle() {
         String word = "beau/belle";
         String expected = "beau";
         String actual = CorrectAnswers.getLeftSideOfSlash(word);
@@ -16,10 +16,61 @@ class CorrectAnswersTest {
     }
 
     @Test
-    void getRightSideOfSlash() {
+    void getLeftSideOfSlash_shouldReturnNull_whenCalledWithLaLiberte() {
+        String word = "la liberté";
+        String expected = null;
+        String actual = CorrectAnswers.getLeftSideOfSlash(word);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getRightSideOfSlash_shouldReturnBelle_whenCalledWithBeauBelle() {
         String word = "beau/belle";
         String expected = "belle";
         String actual = CorrectAnswers.getRightSideOfSlash(word);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void getRightSideOfSlash_shouldReturnNull_whenCalledWithLaLiberte() {
+        String word = "la liberté";
+        String expected = null;
+        String actual = CorrectAnswers.getRightSideOfSlash(word);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getLeftSideOfSlashWithEndingOfRightSide_shouldReturnRoja_whenCalledWithRojoA() {
+        String word = "rojo/-a";
+        String expected = "roja";
+        String actual = CorrectAnswers.getLeftSideOfSlashWithEndingOfRightSide(word);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getLeftSideOfSlashWithEndingOfRightSide_shouldReturnNull_whenCalledWithBeauBelle() {
+        String word = "beau/belle";
+        String expected = null;
+        String actual = CorrectAnswers.getLeftSideOfSlashWithEndingOfRightSide(word);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void
+    getWordWithoutArticle_shouldReturnSueno_whenCalledWithElSuenoAndSpanish(){
+        String word = "el sueño";
+        String expected = "sueño";
+        String actual = CorrectAnswers.getWordWithoutArticle(word, Language.SPANISH);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void
+    getWordWithoutArticle_shouldReturnNull_whenCalledWithElSuenoAndFrench(){
+        String word = "el sueño";
+        String expected = null;
+        String actual = CorrectAnswers.getWordWithoutArticle(word, Language.FRENCH);
+        assertEquals(expected, actual);
+    }
+
 }
